025c6273cf1d38b1fa25fb1e078f66b3533d0814
Themba Lebese <toxiclebese@gmail.com>
2023-02-16 12:04:24 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ * main - a program that uses puts funtion to print a line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
